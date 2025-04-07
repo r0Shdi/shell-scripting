c80401cf88dd8e6eaba54df93573cf4838d06f1a
roshdi <142892591+r0Shdi@users.noreply.github.com>
2025-04-07 19:23:41 +0200

    Add files via upload

---
+++ bash01.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+read -p "Enter the password: " password
+echo $password > password.txt
+
+bcrypt password.txt
+
+
+read -p "Enter the password again: " rpassword
+
+decrypted=$(bcrypt -o password.txt.bfe)
+rm password.txt.bfe
+
+if [ "$decrypted" == "$rpassword" ]; then
+    echo "Password is correct"
+else
+    echo "Password is incorrect"
+fi
+
+
+
+
